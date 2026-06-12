e6386ce06143d31db7630ab3b8dc8371d01405cc
Cyril Braguy <cyril.braguy@gmail.com>
2026-06-12 11:02:50 +0200

    merge dev & Sandra's modifs on streamlit3 3eme

--- Streamlit/streamlit_music_app3.py
+++ Streamlit/streamlit_music_app3.py
@@ -374,7 +374,7 @@ def call_predict_api(list_features_obj: list) -> str:
     try:
         resp = requests.post(API_URL + 'predict_f', json=payload_f, timeout=65)
         resp.raise_for_status()
-        return resp.json()["prediction"][0]
+        return resp.json()["prediction"]
     except Exception as e:
         return f"Erreur API : {e}"
 
